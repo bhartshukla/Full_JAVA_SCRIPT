99d8648a0f4e9729060ad49ab85b696bc5857921
Bhart Shukla <itsmebhsh@gmail.com>
2025-04-12 07:30:00 +0000

    conversion

---
+++ basic/03_conversionoroperation.js
@@ -0,0 +1,52 @@
+// let age1  = "33";
+
+// console.log(typeof age1);
+// console.log(typeof (age1));
+
+// let age2 = Number(age1) // its age convert in number
+
+// console.log(typeof age2 );
+
+
+
+// let score = "3344rr"
+
+// console.log(typeof score);
+
+// let score2 = Number(score)
+
+// console.log(typeof score2);
+// console.log(score2);  // here the value of Number score2 is NaN
+
+
+
+
+// let number = null
+// console.log(typeof number);
+// console.log(number)
+
+// let number2 = Number(null)
+// console.log(typeof number2) // null is changed in nhumber
+// console.log(number2)  // here null value is 0 
+
+
+
+// let bol = true;
+// console.log(typeof bol);
+// console.log(bol)
+// let bol2 = Number(bol)
+// console.log(typeof bol2) // boolean is changed in nhumber
+// console.log(bol2)  // here boolen true  value is 1  and if false so it 0
+
+
+// "20" => 33
+// "20ert" => NaN
+// true => 1
+// false => 0
+
+
+// 1 => boolean true
+// 0 => boolean false
+// "" => boolean false
+// "Bharat" => boolean true
+ 
