111f2f9cdc7fa74d9f3ea05f27f31921d3741f39
Bhart Shukla <itsmebhsh@gmail.com>
2025-04-15 14:28:42 +0000

    math done

---
+++ basic/09_math.js
@@ -0,0 +1,13 @@
+console.log(Math)  //object
+
+console.log(Math.abs(-45));  // negative to positive but not positive to negative
+console.log(Math.round(4.6));
+console.log(Math.ceil(4.3))
+console.log(Math.floor(4.6))
+
+console.log(Math.min(4,5,6,7,8))
+console.log(Math.max(8,6,8,5,4))
+
+
+console.log(Math.random())
+
