9f0142a92b98fe0dcdf76a609a0487ad5bc99d04
Bhart Shukla <itsmebhsh@gmail.com>
2025-04-15 14:21:34 +0000

    number done

---
+++ basic/08_num.js
@@ -0,0 +1,21 @@
+const score = 45;
+console.log(score);
+
+const score2 = new Number(67);
+console.log(score2);
+
+console.log(score2.toString())
+console.log(typeof(score2))
+console.log(score2.toString().length)
+
+console.log(score2.toFixed(3))
+
+const  onumber = 134.55
+
+console.log(onumber.toPrecision(3))
+
+const newnumber = 1000000;
+console.log(newnumber.toLocaleString('en-IN'))
+
+
+
