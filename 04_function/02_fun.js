ebc2146df00aaf6f7dcdbadde79b488b20872f03
Bhart Shukla <itsmebhsh@gmail.com>
2025-05-14 18:41:17 +0000

    function with object and array

--- 04_function/02_fun.js
+++ 04_function/02_fun.js
@@ -12,7 +12,37 @@
 // }
 // console.log(restope(200,400,500,600))
 
-function restope(val,val2 , ...num2){  // here val = 200 , val2 = 400 and num1 = 500,600
-    return num2
+// function restope(val,val2 , ...num2){  // here val = 200 , val2 = 400 and num1 = 500,600
+//     return num2
+// }
+// console.log(restope(200,400,500,600))
+
+
+// function with object
+
+const user = {
+    name:"Bharat",
+    price:200
 }
-console.log(restope(200,400,500,600))
+
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.name} and price is ${anyobject.price}`)
+}
+
+// handleObject(user)
+// direct object 
+handleObject({
+    name:"Bharat",
+    price:300
+})
+
+// function with array
+const myarr = [10,20,30,40]
+
+function findvalue(getArr){
+    return getArr[3]
+}
+
+// console.log(findvalue(myarr))
+
+console.log(findvalue([20,30,20,40]))
