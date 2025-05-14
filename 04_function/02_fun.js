2fcd5ffffc8828d76ea67f5d5d06f1312075317b
Bhart Shukla <itsmebhsh@gmail.com>
2025-05-14 18:30:19 +0000

    rest ope

---
+++ 04_function/02_fun.js
@@ -0,0 +1,18 @@
+// function calculateCartPrice  (num1){
+//     return num1
+// }
+
+// console.log(calculateCartPrice(400))
+
+
+// if we have multi valuese so we use rest operator
+
+// function restope(...num2){
+//     return num2
+// }
+// console.log(restope(200,400,500,600))
+
+function restope(val,val2 , ...num2){  // here val = 200 , val2 = 400 and num1 = 500,600
+    return num2
+}
+console.log(restope(200,400,500,600))
