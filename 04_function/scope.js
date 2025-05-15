84ba6b5bc372c871f56a1a257b85579483d92247
Bhart Shukla <itsmebhsh@gmail.com>
2025-05-15 14:59:20 +0000

    scope

---
+++ 04_function/scope.js
@@ -0,0 +1,16 @@
+
+// var c =400
+
+let a =500
+console.log(a)
+
+if(true){
+    let a =20
+    const b = 30 
+    // var c = 40
+    console.log("Inner", a)
+}
+
+console.log(a)
+// console.log(b)
+// console.log(c)
