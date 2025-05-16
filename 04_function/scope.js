3a9184eb83b8d0d74f0c90cb09eece7ae28efd8f
Bhart Shukla <itsmebhsh@gmail.com>
2025-05-16 06:27:42 +0000

    scope nested

--- 04_function/scope.js
+++ 04_function/scope.js
@@ -2,15 +2,56 @@
 // var c =400
 
 let a =500
-console.log(a)
+// console.log(a)
 
 if(true){
     let a =20
     const b = 30 
     // var c = 40
-    console.log("Inner", a)
+    // console.log("Inner", a)
 }
 
-console.log(a)
+// console.log(a)
 // console.log(b)
 // console.log(c)
+
+// nested scope
+
+
+function one(){
+    const username = "Bharat"
+
+    function two(){
+        const web = "Youtube"
+        console.log(username)
+    }
+    // console.log(website)
+
+    two()
+
+}
+// one()
+
+
+if(true){
+    const username = "bharat"
+
+    if(username==="bharat"){
+        const website = "Youtube"
+        // console.log(username  + website)
+    }
+
+    // console.log(website)
+}
+// console.log(username)
+
+
+function addone(num){
+    return num + 1;
+}
+addone()
+
+const addone2 = function (num){
+    return num + 1;
+}
+addone2()
