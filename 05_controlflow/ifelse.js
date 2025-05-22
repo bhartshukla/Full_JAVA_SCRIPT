03a32545aace2ed778cc8ee4b96d2abbed2c7d44
Bhart Shukla <itsmebhsh@gmail.com>
2025-05-22 06:06:08 +0000

    if else statement

---
+++ 05_controlflow/ifelse.js
@@ -0,0 +1,57 @@
+// if 
+
+// if(true){
+
+// }
+
+
+// const userlogin = true
+
+// if(userlogin){
+//     console.log("UserloggedIn")
+// }
+
+// // < , > , <=,>=,==,!=,=== , !==
+
+// if(2=='2'){
+//     console.log("execute")
+// }
+
+// if(2==='2'){
+//     console.log("execute")
+// }else{
+//     console.log("Not execute")
+// }
+
+
+
+// const bal = 2200
+
+// // if(bal>5000) console.log("good");
+
+// if(bal>5000){
+//     console.log("good")
+// }else if (bal>4000){
+//     console.log("Not bad")
+
+// }else if(bal>2000){
+//     console.log("bad")
+// }else{
+//     console.log("Very bad")
+// }
+
+
+const userloggin = true
+const card = true
+
+const googleloogin = false
+const emaillogin = true
+
+if(userloggin && card){
+    console.log("you can buy product")
+}
+
+
+if(googleloogin || emaillogin){
+    console.log("You are logged")
+}
