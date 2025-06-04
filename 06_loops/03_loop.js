4429a5fd2d365973c29f13e227bedc46bd9395cb
Bhart Shukla <itsmebhsh@gmail.com>
2025-06-04 15:05:14 +0000

    for off loop

--- 06_loops/03_loop.js
+++ 06_loops/03_loop.js
@@ -1,1 +1,15 @@
 // For of Loop
+
+// ["", "", ""]
+// [{},{},{}]
+
+const arr = [1,2,3,4,5]
+for(const num of arr){
+    // console.log(`Value is ${num}`);
+}
+
+
+const greeting = "hellow Bharat"
+for(const greet of greeting){
+    console.log(`each char is ${greet}`)
+}
