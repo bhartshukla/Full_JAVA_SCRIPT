e3bfdbed8b601dd650abe48e1cad8b14f7bded6c
Bhart Shukla <itsmebhsh@gmail.com>
2025-06-12 15:43:47 +0000

    map done

--- 06_loops/07_map.js
+++ 06_loops/07_map.js
@@ -1,6 +1,6 @@
 const mynums = [1,2,3,4,5,6,7,8,9,10]
 
-// const newnums = mynums.map( (num) => num+10)
+const newnums = mynums.map( (num) => num+10) //map returns value
 // console.log(newnums)
 
 const newnum2 = []
@@ -9,3 +9,15 @@ mynums.forEach((num) => {
     newnum2.push(num+10)
 })
 console.log(newnum2)
+
+
+
+
+const bignums = [1,2,3,4,5,6,7,8,9]
+
+const newnum3 = bignums
+                .map( (num) => num*10 )
+                .map((num) => num+1)
+                .filter( (num)=> num>60)
+
+console.log(newnum3)                
