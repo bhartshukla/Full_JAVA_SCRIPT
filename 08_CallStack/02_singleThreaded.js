2790f11a621004700c9188814aa876cf801980c6
Bhart Shukla <itsmebhsh@gmail.com>
2025-06-27 18:17:48 +0000

    javascript is single thred

---
+++ 08_CallStack/02_singleThreaded.js
@@ -0,0 +1,23 @@
+//  let a = 0;
+//  console.log(a)
+
+//  let b= 20;
+//  console.log(b)
+
+//  let c = 30;
+//  console.log(c)
+
+// synchrous Nature
+
+//javascript asynchrous nature
+
+setTimeout (()=>{
+      console.log("Bharat SHUKLA");
+}, 2000);
+
+
+setTimeout (()=>{
+      console.log("its same as same");
+}, 2000);
+
+console.log("Hello ...")
